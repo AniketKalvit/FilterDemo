133ddb0f9ae88e697312f44092c9b8091b7e5323
aniketkalvit <aniketkalvit@gmail.com>
2022-12-02 15:35:18 +0530

    Add project files.

---
+++ FilterDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using FilterDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilterDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [UserLog]
+        public IActionResult EmployeeList()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee{Id=1,Name="emp1",Salary=45000},
+                new Employee{Id=2,Name="emp1",Salary=45000},
+                new Employee{Id=3,Name="emp1",Salary=45000},
+                new Employee{Id=4,Name="emp1",Salary=45000},
+                new Employee{Id=5,Name="emp1",Salary=45000},
+            };
+            ViewBag.Employees = employees;  
+            return View();
+        }
+    }
+}
---
+++ FilterDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using FilterDemo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FilterDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [UserLog]
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        [UserLog]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
